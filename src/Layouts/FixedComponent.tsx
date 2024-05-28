889eb1324dac6ff91ba6983bdef541d33dcc4a35
Andrew Aghoghovwia <androyt1@gmail.com>
2024-05-28 13:51:35 +0100

    Added icons to button

--- src/Layouts/FixedComponent.tsx
+++ src/Layouts/FixedComponent.tsx
@@ -1,4 +1,5 @@
 import SocialIcons from "./SocialIcons";
+import { FaDownload, FaEnvelope } from "react-icons/fa";
 
 const FixedComponent = () => {
     return (
@@ -11,11 +12,13 @@ const FixedComponent = () => {
                         className='min-w-full w-full object-cover'
                     />
                 </div>
-                <div className='h-1/2  min-w-full py-3 space-y-3 px-3 '>
-                    <button className='py-2  font-semibold rounded-md w-full border border-white/50  text-white md:mt-6'>
-                        Download Cv
+                <div className='h-1/2 min-w-full py-3 space-y-3 px-3'>
+                    <button className='flex items-center justify-center py-2 font-semibold rounded-md w-full border border-white/50 text-white md:mt-6 hover:bg-white hover:text-zinc-800 active:bg-gray-200 active:text-zinc-800'>
+                        <FaDownload className='h-5 w-5 mr-2' />
+                        Download CV
                     </button>
-                    <button className='py-2 font-semibold rounded-md w-full bg-[#38BDF8]   text-zinc-800'>
+                    <button className='flex items-center justify-center py-2 font-semibold rounded-md w-full bg-[#38BDF8] text-zinc-800 hover:bg-[#2CA7D8] hover:text-white active:bg-[#1C8DB0] active:text-white'>
+                        <FaEnvelope className='h-5 w-5 mr-2' />
                         Contact Me
                     </button>
                     <SocialIcons />

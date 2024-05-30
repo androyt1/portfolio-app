1d7b43dad0510fb2beb886da7b7ca3716e077b4e
Andrew Aghoghovwia <androyt1@gmail.com>
2024-05-30 09:18:16 +0100

    Add action to buttons

--- src/Layouts/FixedComponent.tsx
+++ src/Layouts/FixedComponent.tsx
@@ -2,6 +2,23 @@ import SocialIcons from "./SocialIcons";
 import { FaDownload, FaEnvelope } from "react-icons/fa";
 
 const FixedComponent = () => {
+    const handleDownload = () => {
+        const link = document.createElement("a");
+        link.href = "/pdf/cv.pdf";
+        link.download = "cv.pdf";
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    };
+
+    const scrollToContact = () => {
+        const link = document.createElement("a");
+        link.href = "#contact";
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    };
+
     return (
         <div className='hidden sm:flex relative sm:w-[250px] md:w-[360px] mr-16'>
             <div className=' fixed top-[80px] h-screen sm:w-[250px] md:w-[360px] flex-col justify-center items-start   overflow-hidden rounded-bl-xl   '>
@@ -9,11 +26,15 @@ const FixedComponent = () => {
                     <img src='/images/me.png' alt='' className='min-w-full w-full object-cover' />
                 </div>
                 <div className='h-1/2 min-w-full py-3 space-y-3 px-3'>
-                    <button className='flex items-center justify-center py-2 font-semibold rounded-md w-full border border-white/50 text-white md:mt-6 hover:bg-white hover:text-zinc-800 active:bg-gray-200 active:text-zinc-800'>
+                    <button
+                        className='flex items-center justify-center py-2 font-semibold rounded-md w-full border border-white/50 text-white md:mt-6 hover:bg-white hover:text-zinc-800 active:bg-gray-200 active:text-zinc-800'
+                        onClick={handleDownload}>
                         <FaDownload className='h-5 w-5 mr-2' />
                         Download CV
                     </button>
-                    <button className='flex items-center justify-center py-2 font-semibold rounded-md w-full bg-[#38BDF8] text-zinc-800 hover:bg-[#2CA7D8] hover:text-white active:bg-[#1C8DB0] active:text-white'>
+                    <button
+                        className='flex items-center justify-center py-2 font-semibold rounded-md w-full bg-[#38BDF8] text-zinc-800 hover:bg-[#2CA7D8] hover:text-white active:bg-[#1C8DB0] active:text-white'
+                        onClick={scrollToContact}>
                         <FaEnvelope className='h-5 w-5 mr-2' />
                         Contact Me
                     </button>

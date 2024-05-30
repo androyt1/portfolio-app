d9bdd879b0a85e89f0b3c943a5ae89d6fa3db420
Andrew Aghoghovwia <androyt1@gmail.com>
2024-05-30 18:06:52 +0100

    fixed mobile padding

--- src/Layouts/Header.tsx
+++ src/Layouts/Header.tsx
@@ -14,7 +14,7 @@ const Header = () => {
     };
 
     return (
-        <header className='h-20 w-full  sticky top-0 flex justify-between items-center px-5 -mt-20 z-[100] backdrop-blur-md '>
+        <header className='h-20 w-full  sticky top-0 flex justify-between items-center px-3 md:px-5 -mt-20 z-[100] backdrop-blur-md '>
             <nav className='flex justify-between items-center w-full relative'>
                 <div className='text-2xl font-noraml text-stone-100 font-inter'>
                     Andrew <span className=''>Aghoghovwia</span>
